ee711d5b88416b9fc48798aa54fe466aa73af82d
mrvictory1509 <55972167+mrvictory1509@users.noreply.github.com>
2019-11-07 13:10:49 +0700

    Update ConnectorSQL.php

--- ConnectorSQL.php
+++ ConnectorSQL.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = pg_connect("host=ec2-107-22-216-53.compute-1.amazonaws.com port=5432 dbname=de1j7no55tqdip user=sdrxherhwxeize password=e52d3c1b396d6331cfbaf2d5768b1e27204ce6076a3739cd3e005388ba60cf5c");  
+$connection = pg_connect("host=ec2-54-221-198-156.compute-1.amazonaws.com port=5432 dbname=dcvm369d6sl8tr user=tpxgiylnxuuodr password=3c1187366d0ec071c50a6b23f08463c7cf5c0e83b0d8304df3d6d76ac0bff16e");  
  if(!$connection) {
      die("Database connection failed");
  }
