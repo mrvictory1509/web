d360c1eea27cffda04d38af5564d12a3801685f9
mrvictory1509 <55972167+mrvictory1509@users.noreply.github.com>
2019-11-07 16:19:44 +0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <title>Log in</title>
-</head> 
-   <body bgcolor = "#0EF08E">
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>         
-            <div style = "margin:30px">
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>           
-            </div>
-         </div>       
-      </div>
-   </body>
-</html>
 <?php
    define('DB_SERVER', 'localhost:3036');
    define('DB_USERNAME', 'root');
@@ -31,19 +9,18 @@
    include("config.php");
    session_start();
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form      
+      // username and password sent from form 
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']);       
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);      
       $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];      
-      $count = mysqli_num_rows($result);
-      // If result matched $myusername and $mypassword, table row must be 1 row	
+      $active = $row['active'];
+          $count = mysqli_num_rows($result);     
+      // If result matched $myusername and $mypassword, table row must be 1 row
       if($count == 1) {
          session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
+         $_SESSION['login_user'] = $myusername;      
          header("location: welcome.php");
       }else {
          $error = "Your Login Name or Password is invalid";
@@ -52,3 +29,47 @@
 ?>
 <html>
    
+   <head>
+      <title>Login</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
+
+   </body>
+</html>
